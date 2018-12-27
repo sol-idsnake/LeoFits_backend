1608b1053e6bd25f1c9a1555ae20d93b4221dbe3
unknown <h.wendt@gmx.com>
2018-12-27 16:19:22 -0500

    updated

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -9,6 +9,37 @@ const Mutations = {
       info
     );
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updartes
+    delete updates.id;
+    // run the update method
+    // ctx is context in the request
+    // db is how we expose prisma database
+    // either query or mutation
+    // updateItem is function from schema.graphql
+    // where is how we tell which item to update
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      // function will expect to return an item,
+      // info is taking the place of the item
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // check if they own that item or have permission
+    // then delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,10 +1,17 @@
-const { forwardTo } = require('prisma-binding')
+const { forwardTo } = require("prisma-binding");
+
+// item: responsible to display single item.
+// TODO: custom resolver to send error to frontend if no single item found
 
 const Query = {
-  items: forwardTo('db')
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  // itemsConnection exposes aggregate data to frontend.
+  // Here to enable pagination
+  itemsConnection: forwardTo("db")
   // async items(parent, args, ctx, info) {
-    // const items = await ctx.db.query.items()
-    // return items
+  // const items = await ctx.db.query.items()
+  // return items
   // }
 };
 
