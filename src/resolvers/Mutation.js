0ddd9b8d69987297a3aa780baddf3d58334d96dc
unknown <h.wendt@gmx.com>
2019-01-07 02:08:23 -0500

    finished cart functionality

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -247,6 +247,34 @@ const Mutations = {
       },
       info
     );
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    // find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      `{id, user {id}}`
+    );
+    // make sure we found an item
+    if (!cartItem) {
+      throw new Error("No cart item found.");
+    }
+    // make sure they own the cart item
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error("Invalid");
+    }
+    // delete that cartItem
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
