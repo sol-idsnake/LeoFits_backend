2409d3d1229bcaf4aa0d19739bb5f52c608a25ef
Hendrik Wendt <h.wendt@gmx.com>
2019-02-17 15:56:25 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const db = require("./db");
 
 const server = createServer();
 
-// TODO use express middleware to handle cookies JWT
+// use express middleware to handle cookies JWT
 server.express.use(cookieParser());
 // TODO use express middleware to populate current User
 
